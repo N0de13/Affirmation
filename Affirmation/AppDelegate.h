bc7aaae3bd474887425409a2edb11782e394a599
Joe Murphy <joemurphy99@icloud.com>
2015-05-07 14:29:16 -0400

    Initial Commit

---
+++ Affirmation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Affirmation
+//
+//  Created by Joseph Murphy on 5/7/15.
+//  Copyright (c) 2015 Joseph Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Affirmation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Affirmation
+//
+//  Created by Joseph Murphy on 5/7/15.
+//  Copyright (c) 2015 Joseph Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
